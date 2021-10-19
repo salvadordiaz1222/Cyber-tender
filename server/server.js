3469bad20c2c547478e287e646abba2e635fd6cf
jpoulin587 <jpoulin587a@gmail.com>
2021-10-19 18:00:27 -0500

    added 'path' to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
+
 
 const mongoConnection = require('./config/connection');
 
