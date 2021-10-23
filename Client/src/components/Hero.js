2ad67a2ef6f291ea3b586b12d378bc0a4a1bac63
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-23 13:14:40 -0500

    more styling

--- Client/src/components/Hero.js
+++ Client/src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div className='neon-container'>
+        <div className='neon-container  grey darken-3'>
             <div className='row'>
                 <h2 className='cyber'>Cyber</h2>
                 <h2 className='cyber2'>Tender</h2>
