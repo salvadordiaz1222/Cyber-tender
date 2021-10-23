ac7ce07fda4d023ebf7f81e491ffe1642692c365
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-10-23 13:16:40 -0500

    working on resolvers for graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,29 +7,65 @@ const resolvers = {
       // Get and return all documents from the classes collection
       return Drinks.find({});
     },
+    options: async (parent, args) => {
+      const { nextKey, options } = args;
+      const dbOpt = options.reduce((r, opt) => {
+        // r['liquor'] = 'Rum'
+        r[opt.key] = opt.value;
+        // r.liquor = 'Rum'
+        return r;
+      }, {});
 
-    
+      /*
+  
+      {
+  
+      }
+  
+      */
 
-//     getDrinks: async (parent, { options, nextKey }) => {
-//       const dbOpt = options.reduce((r, opt) => {
-//         r[opt.key] = opt.value;
+      const responses = await Drinks.find(dbOpt);
 
-//         return r;
-//       }, {});
+      /*
+  
+      let res = response.map(opt => opt[nextKey]) -> ['Rum', 'Rum', 'Tequila', ...]
+      let set  = new Set(res) -> {'Rum', 'Tequila'}
+      let finalUniqueKeys = Array.from(set) -> ['Rum', 'Tequila']
+  
+      */
 
-//       const responses = await Drinks.find(dbOpt);
+      // No more questions send the final results
+      if (!nextKey) {
+        return { data: responses };
+      }
 
-//       if (!nextKey) {
-//         return { data: responses, options: nextOptions };
-//       }
+      const nextOptions = Array.from(
+        new Set(response.map((opt) => opt[nextKey]))
+      );
 
-//       const nextOptions = Array.from(
-//         new Set(response.map((opt) => opt[nextKey]))
-//       );
+      return { data: responses, options: nextOptions };
+    },
 
-//       return { data: responses, options: nextOptions };
-//     },
- },
- };
+    //     getDrinks: async (parent, { options, nextKey }) => {
+    //       const dbOpt = options.reduce((r, opt) => {
+    //         r[opt.key] = opt.value;
+
+    //         return r;
+    //       }, {});
+
+    //       const responses = await Drinks.find(dbOpt);
+
+    //       if (!nextKey) {
+    //         return { data: responses, options: nextOptions };
+    //       }
+
+    //       const nextOptions = Array.from(
+    //         new Set(response.map((opt) => opt[nextKey]))
+    //       );
+
+    //       return { data: responses, options: nextOptions };
+    //     },
+  },
+};
 
 module.exports = resolvers;
