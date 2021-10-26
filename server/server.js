1fc3bc52cb3aa0397a0944962f581965abf4cab0
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-10-26 14:54:52 -0500

    added apolloServer and schemas

--- server/server.js
+++ server/server.js
@@ -18,21 +18,20 @@ const startApolloServer = async () => {
   });
   await server.start();
   server.applyMiddleware({ app });
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+  app.get("*", (req, res) => {
+    console.log(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 };
 
-app.use(routes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+// app.use(routes);
 
 mongoConnection.once("open", () => {
   startApolloServer();
