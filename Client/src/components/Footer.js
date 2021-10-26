cfd79e979b718718483e548dc3d62e86a3cf2011
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-26 00:34:36 -0500

    working on menu

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -4,29 +4,26 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-            <footer className="page-footer">
-          <div className="container">
+            <footer className="page-footer grey darken-4">
+          <div className="container grey darken-3">
             <div className="row">
               <div className="col l6 s12">
-                <h5 className="grey-text">Footer Content</h5>
-                <p className="black-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 className="grey-text">Developers</h5>
+                <p className="grey-text text-lighten-4">Here are some links to look at more of our work, you can also contact us here for questions.</p>
               </div>
               <div className="col l4 offset-l2 s12">
-                <h5 className="grey-text">Links</h5>
+                <h5 className="grey-text">Github Links</h5>
                 <ul>
-                  <li><a className="black-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="black-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a className="black-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a className="black-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://github.com/jpoulin587">Jim Poulin</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://github.com/salvadordiaz1222">Salvador Diaz</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://github.com/LRumgay">Liam Rumgay</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!"></a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div className="footer-copyright">
-            <div className="black-text container">
-            © 2014 Copyright Text
-            <a className="black-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
+            {/* footer copyright content */}
           </div>
         </footer>
         )
--- Client/src/components/Hero.js
+++ Client/src/components/Hero.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div className='neon-container  grey darken-3'>
+        <div className='neon-container grey darken-4'>
             <div className='row'>
                 <h2 className='cyber'>Cyber</h2>
-                <h2 className='cyber2'>Tender</h2>
+                <h2 className='cyber2'>Bar</h2>
             </div>
         </div>
     )
