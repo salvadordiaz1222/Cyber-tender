b5be53a619bf48613196dee9ae767f4814c39fa5
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-10-26 14:52:54 -0500

    resolvers are running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,26 +3,25 @@ const { Drinks, Questions } = require("../models");
 // Create the functions that fulfill the queries defined in `typeDefs.js`
 const resolvers = {
   Query: {
-    Drinks: async () => {
+    drinks: async () => {
       // Get and return all documents from the classes collection
       return Drinks.find({});
     },
-    options: async (parent, args) => {
-      const { nextKey, options } = args;
-      const dbOpt = options.reduce((r, opt) => {
+    questions: async () => {
+      return Questions.find({});
+    },
+  },
+
+  Mutation: {
+    getOptions: async (parent, args) => {
+      const { nextKey, selectedOptions } = args;
+
+      const dbOpt = selectedOptions.reduce((r, opt) => {
         // r['liquor'] = 'Rum'
         r[opt.key] = opt.value;
         // r.liquor = 'Rum'
         return r;
       }, {});
-
-      /*
-  
-      {
-  
-      }
-  
-      */
 
       const responses = await Drinks.find(dbOpt);
 
@@ -36,35 +35,15 @@ const resolvers = {
 
       // No more questions send the final results
       if (!nextKey) {
-        return { data: responses };
+        return { data: responses, options: [] };
       }
 
       const nextOptions = Array.from(
-        new Set(response.map((opt) => opt[nextKey]))
+        new Set(responses.map((opt) => opt[nextKey]))
       );
 
       return { data: responses, options: nextOptions };
     },
-
-    //     getDrinks: async (parent, { options, nextKey }) => {
-    //       const dbOpt = options.reduce((r, opt) => {
-    //         r[opt.key] = opt.value;
-
-    //         return r;
-    //       }, {});
-
-    //       const responses = await Drinks.find(dbOpt);
-
-    //       if (!nextKey) {
-    //         return { data: responses, options: nextOptions };
-    //       }
-
-    //       const nextOptions = Array.from(
-    //         new Set(response.map((opt) => opt[nextKey]))
-    //       );
-
-    //       return { data: responses, options: nextOptions };
-    //     },
   },
 };
 
