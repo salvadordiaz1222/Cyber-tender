c1dfbef33b23b9d465c18adb4d16402d73f8f57b
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-26 20:43:26 -0500

    menu data added for drink menu

---
+++ Client/src/Menudata.js
@@ -0,0 +1,63 @@
+export const drinks = [
+    {
+        "title": "MINT TART",
+        "ingredients": ["2 oz rum", "1 grapefruit", "10 mint leaves", "2 tsp agave nectar", "1 tbsp lime juice", "1 oz grapefruit juice", "Splash of sparkling water"  ],
+        "directions": "Muddle 4 grapefruit segments, mint, agave nectar and lime juice in a cocktail shaker. Add the juice and rum, fill with ice, and shake to blend. Strain into a glass with ice and top with sparkling water.",
+        "image": "./Client/src/Images/MintTart.png"
+     },
+     {
+         "title": "CINNAMON CIDER",
+         "ingredients": ["1 1/4 oz spiced rum", "6 oz hot apple cider", "1 tbsp sugar","1 tsp cinnamon" ],
+         "directions": "Add hot apple cider and spiced rum into a glass rimmed with sugar and cinnamon.",
+         "image": "./Client/src/Images/CinnamonToast.png"
+      },
+      {
+         "title": "HAPPY GO LUCKY",
+         "ingredients": ["2 oz rum", "1/4 oz crème de menthe", "1/2 oz orange liqueur","1/2 oz lime juice" ],
+         "directions": "Add all ingredients into cocktail shaker filled with ice. Shake and strain into a cocktail glass.",
+         "image": "./Client/src/Images/HappyGoLucky.png"
+      },
+      {
+         "title": "THE PHANTOM",
+          "ingredients": ["2 oz white rum", "1 oz absinthe", "3/4 oz simple syrup", "1/2 oz lime juice", "1 tbsp sugar" ],
+         "directions": "Mix sugar and food coloring until it has a thick, runny consistency. Pour onto a plate and dip the rim of a martini glass into it. Add remaining ingredients to an ice-filled cocktail shaker and shake well. Strain into your glass and enjoy!",
+         "image": "./Client/src/Images/The_Phantom.png"
+ 
+      },
+      {
+          "title": "LEMONBERRY SPLASH",
+          "ingredients": ["2 oz white rum", "3 hulled strawberries", "1 oz lemon juice","1 oz simple syrup", "Strawberry for garnish" ],
+          "directions": "Muddle strawberries in a cocktail shaker. Add rum, juice and simple syrup. Fill with ice and shake well.  Strain mixture into a glass filled with ice and garnish with a strawberry. Enjoy!",
+          "image": "./Client/src/Images/LemonberrySplash.jpg"
+       },
+       {
+          "title": "PINA COLADA PUNCH",
+          "ingredients": ["750ml sauvignon blanc", "1/2 cup coconut rum", "2 cups pineapple juice","1 cup seltzer", "1 cup chopped pineapple", "1/4 cup maraschino cherries" ],
+          "directions": "Combine ingredients in a pitcher and stir. Refrigerate until chilled. Garnish with pineapple and serve!",
+          "image": "./Client/src/Images/PinaPunch.jpg"
+       },
+       {
+         "title": "SHAKE THE COCONUT",
+         "ingredients": ["1 1/2 oz vanilla vodka", "1 oz coconut rum", "1/2 oz cream of coconut","Splash of pineapple juice" ],
+         "directions": "Add all ingredients to an ice-filled cocktail shaker. Shake well and strain into a chilled glass.s",
+         "image": "./Client/src/Images/ShaketheCoconut.png"
+      },
+      {
+          "title": "DARK & STORMY",
+          "ingredients": ["6 oz ginger beer", "1 1/2 oz dark rum", "1 tbsp fresh lime juice", "1 tsp grated ginger"  ],
+          "directions": "Pour ingredients into a glass filled with ice and stir. Garnish with a lime wedge and enjoy!",
+          "image": "./Client/src/Images/DarkAndStormy.png"
+       },
+       {
+          "title": "CITRUS BREEZE",
+          "ingredients": ["2 oz coconut rum", "3/4 oz fresh lime juice", "3 oz grapefruit soda","Lime wedge for garnish" ],
+          "directions": "Combine lime juice and rum in a cocktail shaker with ice and shake to blend. Strain into a rocks glass filled with ice and top with grapefruit soda. Garnish with a lime wedge, lean back, and relax!",
+          "image": "./Client/src/Images/CitrusBreeze.png"
+       },
+       {
+         "title": "TROPICAL SUNRISE",
+         "ingredients": ["1 oz light rum", "1 1/2 oz dark rum", "3 oz passion fruit juice", "3 oz orange juice", "1/2 tbsp grenadine", "Orange slice for garnish" ],
+         "directions": "Combine ingredients in a mixing glass and stir. Pour into a glass with ice and garnish with an orange slice.",
+         "image": "./Client/src/Images/TropicalSunrise.jpg"
+      },
+];
